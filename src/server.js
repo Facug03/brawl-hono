53198dbea43edfd23659f7fe4a462333f98e6b34
Facug03 <facundogonzales1550@gmail.com>
2023-01-09 20:10:48 -0300

    test

--- src/server.js
+++ src/server.js
@@ -14,9 +14,3 @@ app.get('/brawlers', async (c) => {
 })
 
 export default app
-// fetch(`https://api.brawlstars.com/v1/players/%23JGCCGY80`, {
-//     headers: {
-//       Authorization:
-//         'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImU2ZDQ1MzIxLTBiNGItNDJhZC1hMjIxLTU5YzEwMWJiYmIyYSIsImlhdCI6MTY3MzI2ODUxNCwic3ViIjoiZGV2ZWxvcGVyLzNhZDRhZDlkLWIwNmEtZjBkYi00YWQyLTJhYzM2MDRlYjU5YiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTkwLjAuMTc5LjE0MyJdLCJ0eXBlIjoiY2xpZW50In1dfQ.Q28ySgUVP1ineQ30KUUV_TiN8LS13Nx2FsGdO991jQ3qw2qxuR5RH-w5i1xOk2M1HUZl4WfnfdI7UgM1Q6jrxA',
-//     },
-//   })
